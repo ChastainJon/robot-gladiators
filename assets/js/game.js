b5f3eee24b314b9e4c0fe03a1f9e2814deeb686b
Jonathan Chastain <chastain.r.jon@gmail.com>
2021-11-08 00:47:23 -0500

    added HP and AD, added outputs for end of round action report

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,44 @@
-function fight(){
-    window.alert("The fight has begun!");
-}
-//fight();
 var playerName = window.prompt("What is your robot's name?");
-//window.alert(playerName);
-console.log(playerName);
-console.log(10+10);
-console.log("Our robot's name is " + playerName);
-var name = "your name";
-console.log(name);
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function(){
+    window.alert("The fight has begun!");
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    if (enemyHealth <= 0){
+        window.alert(enemyName + " has died!");
+    }
+    else{
+        window.alert(enemyName + " still has " + enemyHealth + " health left");
+    }
+    playerHealth = playerHealth - enemyAttack;
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+    if(playerHealth <= 0){
+        window.alert(playerName + "has died!");
+    }
+    else{
+        window.alert(playerName + " still has " + playerHealth + " health left");
+    }
+};
+if(playerHealth > 0 || enemyHealth > 0){
+    fight();
+}
+else{
+    if(enemyHealth <= 0 && playerHealth > 0){
+        window.alert(playerName + " has won the battle");
+    }
+    if(playerHealth <= 0 && enemyHealth > 0){
+        window.alert(enemyName + " has won the battle");
+    }
+    if(playerHealth <= 0 && enemyHealth <=0){
+        window.alert("Both robots have died, the battle is a draw");
+    }
+    
+}
