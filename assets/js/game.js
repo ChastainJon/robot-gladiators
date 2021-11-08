f5757f27e8dda3cde0d2512088e42cbd07ec3f9d
Jonathan Chastain <chastain.r.jon@gmail.com>
2021-11-07 23:44:33 -0500

    added variables to game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+function fight(){
+    window.alert("The fight has begun!");
+}
+//fight();
+var playerName = window.prompt("What is your robot's name?");
+//window.alert(playerName);
+console.log(playerName);
+console.log(10+10);
+console.log("Our robot's name is " + playerName);
+var name = "your name";
+console.log(name);
