26d408dcaa29d125a9de644ff877a01067bc1a86
Jonathan Chastain <chastain.r.jon@gmail.com>
2021-11-08 22:40:53 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,7 +8,6 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName){
-    window.alert("The fight has begun!");
     while(playerHealth > 0 &&enemyHealth > 0){
         var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
         if (promptFight === "skip" || promptFight === "SKIP"){
@@ -20,38 +19,71 @@ var fight = function(enemyName){
                 console.log("playerMoney", playerMoney);
                 break;
             }
-            else{
-                fight();
-            }
         }
-        if(promptFight === "fight" || promptFight === "FIGHT"){
-            enemyHealth = enemyHealth - playerAttack;
-            console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
-            if (enemyHealth <= 0){
-                window.alert(enemyName + " has died!");
-                break;
-            }
-            else{
-                window.alert(enemyName + " still has " + enemyHealth + " health left");
-            }
-            playerHealth = playerHealth - enemyAttack;
-            console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
-            if(playerHealth <= 0){
-                window.alert(playerName + " has died!");
-                break;
-            }
-            else{
-                window.alert(playerName + " still has " + playerHealth + " health left");
-            }
+        //remove enemyHealth
+        enemyHealth = enemyHealth - playerAttack;
+        console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+        //check enemy health
+        if (enemyHealth <= 0){
+            window.alert(enemyName + " has died!");
+            playerMoney = playerMoney + 20;
+            break;
         }
         else{
-            window.alert("You need to choose a valid option. Try again!");
+            window.alert(enemyName + " still has " + enemyHealth + " health left");
+        }
+        //remove playerHealth
+        playerHealth = playerHealth - enemyAttack;
+        console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+        //check playerHealth
+        if(playerHealth <= 0){
+            window.alert(playerName + " has died!");
+            break;
+        }
+        else{
+            window.alert(playerName + " still has " + playerHealth + " health left");
         }
     }
 };
-//fight();
-for(var i=0; i < enemyNames.length; i++){
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+//Start Game Function
+var startGame = function(){
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+    for(var i=0; i < enemyNames.length; i++){
+        if(playerHealth > 0){
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            //debugger;
+            fight(pickedEnemyName);
+        }
+        else{
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }
+    }
+    endGame();
 }
+//End Game Function
+var endGame = function() {
+    window.alert("The game has now ended. Let's see how you did!");
+    //Check players results
+    if(playerHealth > 0){
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else{
+        window.alert("You've lost your robot in battle.")
+    }
+    //Check to see if player wants to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+    if(playAgainConfirm){
+        //restart game
+        startGame();
+    }
+    else{
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+}
+//start game when page loads
+startGame();
