6bbccbd8ff8e2968d4d328a675e523f0bf0784e6
Jonathan Chastain <chastain.r.jon@gmail.com>
2021-11-10 02:09:34 -0500

    fixed multi-case and empty bug for fight-skip

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,24 @@
+var fightOrSkip = function(){
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enther 'FIGHT' or 'SKIP' to choose.");
+    promptFight = promptFight.toLowerCase();
+    if(promptFight === "" || promptFight === null){
+        window.alert("You need to provide a valid answer! Please try again.");
+        return fightOrSkip();
+    }
+    if (promptFight === "skip"){
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+        if(confirmSkip){
+            window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+            playerInfo.money = playerInfo.money - 10;
+            return true;
+        }
+    }
+    return false;
+}
 var fight = function(enemy){
-    while(playerInfo.health > 0 &&enemy.health > 0){
-        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-        if (promptFight === "skip" || promptFight === "SKIP"){
-            window.alert(playerInfo.name + " has chosen to skip the fight!");
-            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-            if(confirmSkip){
-                window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-                playerInfo.money = Math.max(0, playerInfo.money - 10);
-                console.log("playerInfo.money", playerInfo.money);
-                break;
-            }
+    while(playerInfo.health > 0 && enemy.health > 0){
+        if(fightOrSkip()){
+            break;
         }
         //remove enemy.health
         var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
